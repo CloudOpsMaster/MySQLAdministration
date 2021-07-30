f453553d2c1cc1a447293745070d5ae3b12d30c3
Maks0123 <vadimcorporateukr@gmail.com>
2021-07-30 10:04:14 +0300

    dump

--- dump.sh
+++ dump.sh
@@ -1,23 +1,31 @@
 #!/bin/sh
 
-CURRENTDATE=`date +"%Y-%m-%d %T"`
-path='/home/felix/backup/'
+CURRENTDATE=$(date +"%Y-%m-%d %T")
+path='dumps'
 
 temp1=$(echo $CURRENTDATE | tr "-" "_")
 datafile=$(echo $temp1 | tr ":" "_")
 
-UP=$(/etc/init.d/mysql status | grep running | grep -v not | wc -l);
-if [ "$UP" -ne 1 ];
-then
-        echo "MySQL is down.";
-        echo "Running mysql server ..."
-        service mysql start
-        echo "MySQL is UP.";
 
+if [ -d "$path" ]; then
+    echo "Dir ${path} exist"
 else
-        echo "All is well.";
+    ###  Control will jump here if $DIR does NOT exists ###
+    echo "Dir ${path} not exist!"
+    echo "Create dir dumps!"
+    sudo mkdir dumps
+    exit 1
 fi
 
+UP=$(/etc/init.d/mysql status | grep running | grep -v not | wc -l)
+if [ "$UP" -ne 1 ]; then
+    echo "MySQL is down."
+    echo "Running mysql server ..."
+    service mysql start
+    echo "MySQL is UP."
 
+else
+    echo "All is well."
+fi
 
-mysqldump --login-path=local moodle > $path/moodle_"$datafile".sql
+mysqldump --login-path=local moodle >$path/_"$datafile".sql
