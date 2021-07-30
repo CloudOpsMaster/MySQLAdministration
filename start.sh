394593e7b280f6b9ba5a5392688b57256f4cdbc6
Maks0123 <vadimcorporateukr@gmail.com>
2021-07-30 10:39:58 +0300

    start script

--- start.sh
+++ start.sh
@@ -1,1 +1,34 @@
 #!/bin/bash
+
+echo "1) Install prod server"
+echo "2) Install replica server"
+echo "3) Configur dump to dir dumps"
+
+
+read VALUE
+
+case $VALUE in
+
+1)
+    echo "Install prod server"
+    sudo bash prod.sh
+    echo "Success!"
+    ;;
+
+2)
+    echo "Install replica server"
+    sudo bash replica.sh
+    echo "Success!"
+    ;;
+
+3)
+    echo "Dumps"
+    terraform plan
+    echo "Success!"
+    ;;
+
+
+*)
+    echo "Incorect value"
+    ;;
+esac
